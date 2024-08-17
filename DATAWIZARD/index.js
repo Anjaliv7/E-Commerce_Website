cd231e4cc618bef6ca428c97e9f7300b2e938775
Anjaliv7 <anjali.verma4031@gmail.com>
2024-08-17 22:39:08 +0530

    Add files via upload
    This is my first E-Commerce Website

---
+++ DATAWIZARD/index.js
@@ -0,0 +1,21 @@
+const mobile_nav=document.querySelector(".mobile-navbar-btn");
+const nav_header=document.querySelector(".header");
+const toggleNavbar=()=>{
+nav_header.classList.toggle("active");
+};
+
+
+mobile_nav.addEventListener('click',()=>toggleNavbar());
+
+// /////embedding camera on button
+
+
+// console.log(btn_try)
+
+var try_on=document.querySelector('.tryon');
+console.log(try_on)
+
+
+function Camera_On(){
+    
+}
